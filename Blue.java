fcc6977e9b4bf8d7e45936a9f1566938a158d147
shiv <shivasharanb1989@gmail.com>
2016-06-17 00:04:11 -0700

    Update Blue.java

--- Blue.java
+++ Blue.java
@@ -4,7 +4,10 @@ public class Blue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("Blue dark colour");
+	for(int i=0;i<10;i++)
+	{
+		System.out.println("hello");
+	}
 
 	}
 
