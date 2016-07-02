e1795ea3934fc1e69967722531c3c555838992ed
shiv <shivasharanb1989@gmail.com>
2016-07-01 22:19:28 -0800

    Update Blue.java

--- Blue.java
+++ Blue.java
@@ -4,7 +4,7 @@ public class Blue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	for(int i=0;i<10;i++)
+	for(int i=0;i<5;i++)
 	{
 		System.out.println("hello");
 	}
